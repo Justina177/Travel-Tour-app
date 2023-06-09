ab76f0ff1837903921a1ba67ff8c146975ba62a3
justina177 <justinaenenche@yahoo.com>
2023-06-09 10:14:11 +0100

    Added styles to the Navbar component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Navbar from "./Components/Navbar/Navbar";
 import Home from "./Components/Home/Home"
 import Main from "./Components/Main/Main";
 import Footer from "./Components/Footer/Footer";
-
+import './App.css'
 
 function App() {
   return (
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import './navbar.css';
+import './navbar.scss';
+import { MdOutlineTravelExplore } from 'react-icons/md';
+import { TbGridDots } from 'react-icons/tb';
+import { AiFillCloseCircle } from 'react-icons/ai';
+
 
 
 const Navbar = () => {
@@ -8,8 +12,45 @@ const Navbar = () => {
             <header className="header flex">
                 <div className="logoDiv">
                     <a href="/" className="logo flex">
-                        <h1>Travel</h1>
+                        <h1><MdOutlineTravelExplore className="icon" />Travel</h1>
                     </a>
+                </div>
+
+                <div className="navBar">
+                    <ul className="navLists flex">
+                        <li className="navItem">
+                            <a href="/" className="navLink">Home</a>
+                        </li>
+                        <li className="navItem">
+                            <a href="/" className="navLink">Packages</a>
+                        </li>
+                        <li className="navItem">
+                            <a href="/" className="navLink">Shop</a>
+                        </li>
+                        <li className="navItem">
+                            <a href="/" className="navLink">About</a>
+                        </li>
+                        <li className="navItem">
+                            <a href="/" className="navLink">pages</a>
+                        </li>
+                        <li className="navItem">
+                            <a href="/" className="navLink">News</a>
+                        </li>
+                        <li className="navItem">
+                            <a href="/" className="navLink">Contact</a>
+                        </li>
+                        <button className="btn">
+                            <a href="/">Book Now</a>
+                        </button>
+                    </ul>
+                </div>
+
+                <div className="closeNavbar">
+                    <AiFillCloseCircle className="icon" />
+                </div>
+
+                <div className="toggleNavbar">
+                    <TbGridDots className="icon" />
                 </div>
             </header>
         </section>
